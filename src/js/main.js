428a7534933f1df4c3df8009903324df2c0d986f
mila <vorga88@gmail.com>
2018-09-20 16:51:19 +0300

    add css for sections on index page on mobile, js for opening mobile-menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
 import "../scss/main.scss";
+
+window.addEventListener('load', function() {
+    function MobileMenu () {
+        let el =  document.querySelector(".js-mobile-menu");
+        let open = document.querySelector(".js-top-menu__open");
+        let close = el.querySelector(".js-mobile-menu__close");
+
+        let opened = false;
+
+        function openMenu() {
+            el.style.display = "block";
+        }
+        function closeMenu() {
+            el.style.display = "none";
+        }
+
+        if(el && open && close) {
+            open.addEventListener('click', () =>  { 
+                opened ? closeMenu() : openMenu();
+
+                opened != opened;
+            });
+            close.addEventListener('click', () => {
+                closeMenu();
+                opened = false;
+            });            
+        }
+    }
+
+    new MobileMenu();
+});

40f8a8146fb7cf278812eadfb9ca0cb4733a94b9
mila <vorga88@gmail.com>
2018-09-23 17:03:30 +0300

    add html, css, js for popups

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import "../scss/main.scss";
 
-window.addEventListener('load', function() {
-    function MobileMenu () {
-        let el =  document.querySelector(".js-mobile-menu");
+window.addEventListener('load', function () {
+    function MobileMenu() {
+        let el = document.querySelector(".js-mobile-menu");
         let open = document.querySelector(".js-top-menu__open");
         let close = el.querySelector(".js-mobile-menu__close");
 
@@ -11,12 +11,13 @@ window.addEventListener('load', function() {
         function openMenu() {
             el.style.display = "block";
         }
+
         function closeMenu() {
             el.style.display = "none";
         }
 
-        if(el && open && close) {
-            open.addEventListener('click', () =>  { 
+        if (el && open && close) {
+            open.addEventListener('click', () => {
                 opened ? closeMenu() : openMenu();
 
                 opened != opened;
@@ -24,9 +25,56 @@ window.addEventListener('load', function() {
             close.addEventListener('click', () => {
                 closeMenu();
                 opened = false;
-            });            
+            });
         }
     }
 
+    function Popup(el) {
+
+        if (!el) {
+            return;
+        }
+
+        let closeButton = el.querySelector(".js-popup-button");
+
+        this.openPopup = function () {
+            el.style.display = "flex";
+        }
+
+        this.closePopup = function () {
+            el.style.display = "none";
+        }
+
+        el.addEventListener('click', (e) => {
+            if (e.target == el || e.target == closeButton) {
+                this.closePopup();
+            }
+        });
+    }
+
     new MobileMenu();
+
+
+    let form = document.querySelector(".form");
+
+    if (form) {
+
+        let success = new Popup(document.querySelector(".js-popup-success"));
+        let failure = new Popup(document.querySelector(".js-popup-failure"));
+
+        form.addEventListener('submit', (e) => {
+            let invalid = [...form.elements].filter(el => {
+                return !el.validity.valid;
+            });
+            if (invalid.length == 0) {
+                success.openPopup();
+            } else {
+                failure.openPopup();
+            }
+            e.preventDefault();
+        });
+
+    }
+
+
 });
